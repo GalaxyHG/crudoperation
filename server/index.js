6228a8b08d02ec7cc0ba363d9ac3969ffe8889ee
HELENA_SANTOS_REZENDE <72400684@DELTAB32.BARROCA>
2024-06-10 20:49:01 -0300

    server port change, mongoose conn missing

--- server/index.js
+++ server/index.js
@@ -7,10 +7,29 @@ app.unsubscribe(cors())
 
 const PORT = process.env.PORT || 8080
 
-mongoose.connect("mongodb://localhost:27018/CRUD Operation")
-
-app.get("/",(req,res)=>{
-    res.json({message : "Server is running crud"})
+//Schema
+const schemaData = mongoose.Schema({
+    name : String,
+    email : String,
+    mobile : Number,
+},{
+    timestamps : true   
 })
 
-app.listen(PORT,()=>console.log("Server is running"))
+const userModel = mongoose.model("user",schemaData)
+
+app.get("/",async(req,res)=>{
+    const data = await userModel.find({})
+    
+    res.json({message : success > true, data : data})
+})
+
+mongoose.connect("mongodb://127.0.0.1:27017/crudoperation")
+.then(()=>{
+    console.log("connect to DB")
+    app.listen(PORT,()=>console.log("Server is running"))
+})
+.catch((err)=>console.log(err))
+
+
+
